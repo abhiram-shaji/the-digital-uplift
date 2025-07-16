90927c12b146906c43e87b278f1a94e60ec0b83c
Abhiram <write4abhiram@gmail.com>
2025-07-16 04:28:47 -0700

    feat : create about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ServicesSection from "@/components/sections/ServicesSection";
+import AboutSection from "@/components/sections/AboutSection";
 
 export default function Home() {
   return (
     <main>
       <ServicesSection />
+      <AboutSection />
     </main>
   );
 }
