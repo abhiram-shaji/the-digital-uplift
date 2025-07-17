bc34c71514c8bc92e6756ecd4e27caa520a64605
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2025-07-17 16:45:05 -0700

    fix : vercel deployment time errors

--- src/components/ui/BookingModal.tsx
+++ src/components/ui/BookingModal.tsx
@@ -7,7 +7,6 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
 import { Phone, Video } from "lucide-react";
 
 export default function BookingModal() {
