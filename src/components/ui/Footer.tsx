0a82f7ad90725f8af037d06c1dd1684ae945f223
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2025-07-17 17:25:07 -0700

    Update Footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              LinkedIn
+              Founder
             </a>
           </Button>
         </div>
