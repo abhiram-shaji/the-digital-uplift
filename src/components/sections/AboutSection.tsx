ecbeb4bae3e21eab45963b7882736d19731d80d3
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2025-07-18 00:59:28 -0700

    Update AboutSection.tsx

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -2,7 +2,7 @@
 import { TextGenerateEffect } from "../ui/text-generate-effect";
 import { Card} from "@/components/ui/card";
 
-const words = `Our mission is to help Canadian small businesses grow with a strong digital presence. At The Digital Uplift, you work directly with the designers and developers who bring your vision to life. We believe every business deserves smart tools, clean design, and the visibility to reach more customers online without overpriced agencies or middlemen.`;
+const words = `We are based in Victoria, British Columbia and our mission is to help Canadian small businesses grow with a strong digital presence. At The Digital Uplift, you work directly with the designers and developers who bring your vision to life. We believe every business deserves smart tools, clean design, and the visibility to reach more customers online without overpriced agencies or middlemen.`;
 
 export default function AboutSection() {
   return (
