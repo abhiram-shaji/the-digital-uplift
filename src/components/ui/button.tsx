93e533e6e5e689d0d9f37e41fbe246b8ead5b1c8
Abhiram <write4abhiram@gmail.com>
2025-08-06 23:36:28 -0700

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "border bg-background text-foreground shadow-xs hover:bg-primary hover:text-primary-foreground dark:bg-input/30 dark:border-input dark:hover:bg-primary dark:hover:text-primary-foreground",
+          "border bg-red-800 text-white shadow-xs hover:bg-green-800",
 
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/80 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
