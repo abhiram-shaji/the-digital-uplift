4e288740010533da5e270112d2e4fec61c84e0ad
Abhiram <122574760+abhiram-shaji@users.noreply.github.com>
2025-08-07 01:16:41 -0700

    Update Hero.tsx

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -21,11 +21,11 @@ const words = [
     <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 min-h-screen text-center">
       <header>
         <h1 className="text-5xl md:text-6xl font-bold text-gray-900 dark:text-gray-100 mb-8 leading-tight">
-          <FlipWords words={words} /><br/> Website Starting at{" "}
-          <span className="text-[#f9bc60]">$299 CAD</span>
+          <FlipWords words={words} /> Websites<br/>
+          Built for Performance and Elegance
         </h1>
         <p className="text-2xl md:text-3xl dark:text-gray-100 text-gray-700 mb-10">
-        Book a free appointment now!
+          Book a free appointment now and discover how we craft blazing-fast, elegant, and scalable websites that elevate your brand and convert better.
         </p>
       </header>
       <BookingModalTrigger />
@@ -34,3 +34,5 @@ const words = [
 };
 
 export default Hero;
+
+
