d299290b6999f6ce068677dc196a5880120d022c
Abhiram <write4abhiram@gmail.com>
2025-08-07 19:37:35 -0700

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -43,7 +43,7 @@ function Button({
 
   return (
     <div className="relative inline-flex group">
-      <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt pointer-events-none" />
+      <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-sm group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt pointer-events-none" />
       <Comp
         className={cn(
           "relative inline-flex items-center justify-center text-white font-bold transition-all duration-200 bg-gray-900 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 cursor-pointer",
